449489a30d66d5bc6338c53fe6768f295c06d281
Zack12311 <k.brycko@gmail.com>
2017-01-06 15:10:19 +0100

    second screen going back from screen to screen

---
+++ app/src/main/java/com/zack/a/tutorials/DisplayMessageActivity.java
@@ -0,0 +1,50 @@
+package com.zack.a.tutorials;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        if(message.isEmpty()) {
+            textView.setText("empty");
+        }else{
+            textView.setText(message);
+        }
+
+        Button button = new Button(this);
+        button.setText("Ok");
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goBack(v);
+            }
+        };
+
+        button.setOnClickListener(listener);
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
+        layout.addView(textView);
+        layout.addView(button);
+    }
+
+    public void goBack(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/zack/a/tutorials/MainActivity.java
+++ app/src/main/java/com/zack/a/tutorials/MainActivity.java
@@ -1,13 +1,28 @@
 package com.zack.a.tutorials;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.zack.a.tutorials.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
